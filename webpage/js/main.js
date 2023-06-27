a95bb1d5fbb2a794ba4e2769bea86fe8b017deba
aazamandius1 <45124743+aazamandius1@users.noreply.github.com>
2023-06-27 20:00:41 +0300

    first real stuff

---
+++ webpage/js/main.js
@@ -0,0 +1,25 @@
+let inputField = document.getElementById('inputField'); 
+const addTaskButton = document.getElementById('addTaskButton');
+
+
+addTaskButton.addEventListener( 'click', function(){
+  let taskText = inputField.value.trim();
+
+  if (taskText !== "") {
+    createTask(taskText);
+    inputField.value = "";
+  }
+})
+
+function createTask (taskText) {
+  const defaultStatus = 'inbox';
+  const defaultPriority = 1;
+
+  const newTask = { 
+    name: taskText,
+    status: defaultStatus,
+    priority: defaultPriority };
+  
+    return newTask;
+    
+}
